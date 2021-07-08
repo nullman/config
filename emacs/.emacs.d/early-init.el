2f028a5fd4a8bca14f4963bd6e20e1fa7d435de9
Kyle Sherman <kylewsherman@gmail.com>
2021-07-08 17:14:03 -0500

    Update

---
+++ emacs/.emacs.d/early-init.el
@@ -0,0 +1,22 @@
+;; [[file:init-emacs.org::*Early Init][Early Init:1]]
+;; -*- mode: emacs-lisp; lexical-binding: t; no-byte-compile: t -*-
+;;==============================================================================
+;;; early-init.el
+;;
+;;; Early Emacs Initialization File
+;;
+;; Author: Kyle W T Sherman
+;;
+;; This file was generated from init-emacs.org and should not be edited
+;; manually.
+;;
+;; init-emacs.org => early-init.el
+;;==============================================================================
+
+;; disable default emacs package handling (needed for straight)
+(setq package-enable-at-startup nil)
+
+;;==============================================================================
+;;; early-init.el ends here
+;;==============================================================================
+;; Early Init:1 ends here
