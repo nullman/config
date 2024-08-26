a1923fa894cb0c5da8ae4cc456cd6fade6b85847
Kyle Sherman <kylewsherman@gmail.com>
2024-08-25 17:45:47 -0700

    Update

--- custom/tank/.guix-etc/config.scm
+++ custom/tank/.guix-etc/config.scm
@@ -57,7 +57,11 @@
 
 (operating-system
  (kernel linux)
- (kernel-arguments '("modprobe.blacklist=nouveau"))
+ (kernel-arguments
+  (list
+   "nvidia_drm.modeset=1"
+   "rd.driver.blacklist=nouveau"
+   "modprobe.blacklist=nouveau"))
  (initrd microcode-initrd)
  (firmware (list linux-firmware))
 
@@ -66,59 +70,81 @@
  (locale "en_US.utf8")
  (keyboard-layout (keyboard-layout "us" #:options '("shift:both_capslock" "caps:ctrl_modifier")))
 
- (bootloader (bootloader-configuration
-              (bootloader grub-efi-bootloader)
-              (targets '("/boot/efi"))
-              (keyboard-layout keyboard-layout)))
+ (bootloader
+  (bootloader-configuration
+   (bootloader grub-efi-bootloader)
+   (targets '("/boot"))
+   (keyboard-layout keyboard-layout)))
 
  ;; (bootloader (bootloader-configuration
  ;;              (bootloader grub-bootloader)
  ;;              (targets '("/dev/DEVICE"))
  ;;              (keyboard-layout keyboard-layout)))
 
- (mapped-devices (list
-                  (mapped-device
-                   (source (uuid "c0997efd-36fa-4bb7-9922-8a7cc6832837"))
-                   ;;(source "/dev/nvme0n1p2")
-                   (target "luks-root")
-                   (type luks-device-mapping))
-                  (mapped-device
-                   (source (uuid "5c93d084-63b5-4298-b9ee-a879aa7cc65e"))
-                   ;;(source "/dev/nvme0n1p1")
-                   (target "luks-home")
-                   (type luks-device-mapping))))
+ (mapped-devices
+  (list
+   (mapped-device
+    (source (uuid "c0997efd-36fa-4bb7-9922-8a7cc6832837"))
+    ;;(source "/dev/nvme0n1p2")
+    (target "luks-root")
+    (type luks-device-mapping))
+   (mapped-device
+    (source (uuid "5c93d084-63b5-4298-b9ee-a879aa7cc65e"))
+    ;;(source "/dev/nvme0n1p1")
+    (target "luks-home")
+    (type luks-device-mapping))))
 
- (file-systems (cons*
-                (file-system
-                 (device (uuid "f9ef2e21-bcac-4281-aa0c-eb1b64212e25"))
-                 ;;(device "/dev/mapper/luks-root")
-                 (mount-point "/")
-                 (type "ext4")
-                 (flags '(no-atime))
-                 (dependencies mapped-devices))
-                (file-system
-                 (device (uuid "17A9-4856" 'fat))
-                 ;;(device "/dev/nvme0n1p1")
-                 (mount-point "/boot/efi")
-                 ;;(mount-point "/boot")
-                 (type "vfat"))
-                (file-system
-                 (device (uuid "1865f4f0-2ac7-4659-8688-1d3af93bb5e7"))
-                 ;;(device "/dev/mapper/luks-home")
-                 (mount-point "/home")
-                 (type "ext4")
-                 (flags '(no-atime))
-                 (dependencies mapped-devices))
-                %base-file-systems))
+ (file-systems
+  (cons*
+   (file-system
+    (device (uuid "f9ef2e21-bcac-4281-aa0c-eb1b64212e25"))
+    ;;(device "/dev/mapper/luks-root")
+    (mount-point "/")
+    (type "ext4")
+    (flags '(no-atime))
+    (dependencies mapped-devices))
+   (file-system
+    (device (uuid "17A9-4856" 'fat))
+    ;;(device "/dev/nvme0n1p1")
+    (mount-point "/boot")
+    ;;(mount-point "/boot/efi")
+    (type "vfat"))
+   (file-system
+    (device (uuid "1865f4f0-2ac7-4659-8688-1d3af93bb5e7"))
+    ;;(device "/dev/mapper/luks-home")
+    (mount-point "/home")
+    (type "ext4")
+    (flags '(no-atime))
+    (dependencies mapped-devices))
+   %base-file-systems))
 
- (users (cons*
-         (user-account
-          (name "user")
-          (comment "User")
-          (password (crypt "user" "user"))
-          (group "users")
-          (supplementary-groups '("wheel" "netdev" "audio" "video" "disk" "input")))
-         %base-user-accounts))
+ (users
+  (cons*
+   (user-account
+    (name "user")
+    (comment "User")
+    (password (crypt "user" "user"))
+    (group "users")
+    (supplementary-groups
+     (list
+      ;;"adbusers"
+      "audio"
+      "cdrom"
+      "disk"
+      ;;"docker"
+      "floppy"
+      "input"
+      "kvm"
+      ;;"libvirtd"
+      "lp"
+      ;;"mlocate"
+      "netdev"
+      ;;"networkmanager"
+      "scanner"
+      "users"
+      "video"
+      "wheel")))
+   %base-user-accounts))
 
  (packages
   (append
@@ -191,17 +217,18 @@
    (list (replace-mesa glmark2))
    %base-packages))
 
- (services (cons*
-            (service nvidia-service-type)
-            (service gnome-desktop-service-type
-                     (gnome-desktop-configuration (gnome (replace-mesa gnome))))
-            (service xfce-desktop-service-type)
-            (set-xorg-configuration
-             (xorg-configuration
-              (keyboard-layout keyboard-layout)
-              (modules (cons nvda %default-xorg-modules))
-              (drivers '("nvidia"))))
-            %desktop-services))
+ (services
+  (cons*
+   (service nvidia-service-type)
+   ;;(service gnome-desktop-service-type
+   ;;         (gnome-desktop-configuration (gnome (replace-mesa gnome))))
+   (service xfce-desktop-service-type)
+   (set-xorg-configuration
+    (xorg-configuration
+     (keyboard-layout keyboard-layout)
+     (modules (cons nvda %default-xorg-modules))
+     (drivers '("nvidia"))))
+   %desktop-services))
 
  (name-service-switch %mdns-host-lookup-nss))
 
