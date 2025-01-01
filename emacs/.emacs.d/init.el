edcd5fd136ee31c42b5c73893510b9c6d76d0679
Kyle Sherman <kylewsherman@gmail.com>
2024-12-31 21:37:12 -0600

    Update

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,4 @@
-﻿;; [[file:init-emacs.org::*Init][Init:1]]
+;; [[file:init-emacs.org::#init-init][Init:1]]
 ;; -*- mode: emacs-lisp; lexical-binding: t; no-byte-compile: t -*-
 ;;==============================================================================
 ;;; init.el
